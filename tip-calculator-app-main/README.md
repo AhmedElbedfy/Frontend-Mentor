d0efd5ee7f186a843aeaa362cfef67d04891309c
Ahmed Khaled Elbedfy <ahmed.khaled.hussain@gmail.com>
2021-07-28 13:43:22 +0200

    Add tip calculator app main project

---
+++ tip-calculator-app-main/README.md
@@ -0,0 +1,75 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+## My process
+
+### Built with
+
+- HTML
+- CSS
+  - Flexbox
+  - CSS Grid
+  - Media Query
+- JavaScript
+
+### What I learned
+
+I didn't know how to remove the up and down arrow from the input type number before
+
+```css
+/* for removing the up and down arroe in the input type number */
+/* ref --> https://stackoverflow.com/questions/26024771/styling-an-input-type-number */
+/* Chrome */
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+/* Mozila */
+input[type="number"] {
+  -moz-appearance: textfield;
+  appearance: textfield;
+  margin: 0;
+}
+```
+
+Also I tried to add padding to the input but it didn't work until I added this some border box code
+
+```css
+/* refrence --> https://stackoverflow.com/questions/6586302/add-padding-to-html-text-input-field */
+padding-right: 5px;
+-webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
+-moz-box-sizing: border-box; /* Firefox, other Gecko */
+box-sizing: border-box; /* Opera/IE 8+ */
+```
+
+input event listener was new to me I was using onchange especially with React before but this my first time I use input
+
+```js
+Element.addEventListener("input", fun());
+```
+
+
+## Author
+
+- LinkedIn - [Ahmed Khaled](https://www.linkedin.com/in/ahmedelbedfy/)
---
+++ tip-calculator-app-main/index.js
@@ -0,0 +1,189 @@
+// inputs
+const bill = document.getElementById("bill"); // number taken from the user all bill
+const nOfPeople = document.getElementById("people-number"); // number of people
+
+// tip and total showing places
+const tip = document.getElementById("tip");  // show tip 
+const total = document.getElementById("total"); // show total
+
+// reset button
+const reset = document.getElementById("reset"); // reset button
+
+// tip buttons
+const tipButtons = document.querySelectorAll(".tip"); // all tip buttons
+
+// Custom input Doms
+const customInput = document.getElementById("custom")
+const customLabel = document.getElementById("custom-label")
+
+/* 
+    Global Vars
+*/
+
+let billNumber = 0;
+let tipNumber = 0;
+let numberOfPeople = 0;
+
+// event lisener for tip buttons 
+for (let i = 0; i < tipButtons.length; i++) {
+    tipButtons[i].addEventListener("click", handleTipButton)
+}
+
+// inputs On Change
+bill.addEventListener("input", (e) => {
+    billNumber = e.target.value;
+    updateUI();
+})
+
+nOfPeople.addEventListener("input", (e) => {
+    numberOfPeople = e.target.value;
+    updateUI();
+
+})
+
+customInput.addEventListener("input", e => {
+    let customTipValue = e.target.value;
+
+    if (customTipValue >= 0) {
+        tipNumber = customTipValue / 100
+    }
+
+    updateCustomButton(customTipValue)
+    updateUI();
+
+})
+
+// updateUI every time input change
+function updateUI() {
+    if (numberOfPeople <= 0) {
+        document.querySelector(".warning").style.display = "block";
+        nOfPeople.style.border = "1px solid rgb(201, 9, 9)";
+    } else if (numberOfPeople > 0) {
+
+        const result = getResult(billNumber, tipNumber, numberOfPeople);
+
+
+        total.innerHTML = `$${result.total.toFixed(2)}`;
+        tip.innerHTML = `$${result.tipAmount.toFixed(2)}`;
+
+        document.querySelector(".warning").style.display = "none";
+        nOfPeople.style.border = "none";
+
+    }
+
+    updateDisableButton();
+}
+
+
+// get the result it get call every time input change
+function getResult(bill, tipPercent, nOfP) {
+
+    let tip = tipPercent * bill;
+    let total = (parseFloat(bill) + parseFloat(tip)) / nOfP
+    let tipAmount = tip / nOfP;
+
+    return { total, tipAmount };
+}
+
+/* 
+    helper Functions
+*/
+
+
+function updateCustomButton(customTipValue) {
+    console.log(customTipValue)
+    if (customTipValue >= 0 || customTipValue === "") {
+        customLabel.style.opacity = 0;
+        customInput.style.borderColor = "hsl(172, 70%, 45%)";
+    } else if (customTipValue < 0) {
+        customLabel.style.opacity = "100%";
+        customInput.style.borderColor = "#f3f8fb";
+    }
+}
+
+// when tip button click put active class on it
+function handleTipButton(e) {
+    // remove class from every tip button before put the active class on the clicked one
+    tipButtons.forEach(button =>
+        button.classList.remove("active-tip")
+    )
+
+    // if the button isn't the custom remove the custom active class and the opposite
+    if (e.target.id !== "custom") {
+        customInput.value = ""
+        customLabel.style.opacity = "100%";
+        customInput.style.borderColor = "#f3f8fb";
+    } else if (e.target.id === "custom") {
+        customLabel.style.opacity = 0;
+        customInput.style.borderColor = "hsl(172, 70%, 45%)";
+    }
+
+    // add the active class for the clicked button
+    e.target.classList.add("active-tip")
+
+    // assign the tip number to it's chosen value
+    let tipValue = e.target.value;
+    switch (tipValue) {
+        case "5%":
+            tipNumber = 0.05
+            break;
+        case "10%":
+            tipNumber = 0.1
+            break;
+        case "15%":
+            tipNumber = 0.15
+            break;
+        case "25%":
+            tipNumber = 0.25
+            break;
+        case "50%":
+            tipNumber = 0.5
+            break;
+        default:
+            break;
+    }
+
+    // update the UI with the chossen value
+    updateUI();
+}
+
+/*
+    RESET BUTTON
+*/
+
+// Reset button Functionality
+reset.addEventListener("click", () => {
+
+    // update state
+    billNumber = 0;
+    tipNumber = 0;
+    numberOfPeople = 0;
+
+    // update UI
+    total.innerHTML = `$0.00`;
+    tip.innerHTML = `$0.00`;
+
+    // update input 
+    bill.value = ``
+    nOfPeople.value = ``
+
+    // remove active style from tip Buttons
+    tipButtons.forEach(button =>
+        button.classList.remove("active-tip")
+    )
+
+    customInput.value = ""
+    customLabel.style.opacity = "100%";
+    customInput.style.borderColor = "#f3f8fb";
+})
+
+function updateDisableButton() {
+    // Disable the reset button if there it's no input
+    if (numberOfPeople <= 0 || billNumber <= 0) {
+        reset.disabled = true
+    } else {
+        reset.disabled = false
+    }
+}
+
+
